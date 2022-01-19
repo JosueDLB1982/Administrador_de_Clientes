9fdb38eca7faf8c23e02568ae610f26da7b929ce
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-19 12:54:34 -0300

    El componenete Link en React Router 6

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 const Layout = () => {
     return (
@@ -7,19 +7,19 @@ const Layout = () => {
                 <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
 
                 <nav className="mt-10">
-                    <a
+                    <Link
                         className="text-white text-2xl block mt-2 hover:text-blue-300"
-                        href="/clientes"
+                        to="/clientes"
                     >
                         Clientes
-                    </a>
+                    </Link>
 
-                    <a
+                    <Link
                         className="text-white text-2xl block mt-2 hover:text-blue-300"
-                        href="/clientes/nuevo"
+                        to="/clientes/nuevo"
                     >
                         Nuevo Cliente
-                    </a>
+                    </Link>
                 </nav>
             </div>
 
