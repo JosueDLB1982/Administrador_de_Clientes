ed188f8f848761736707bc1101ea49dc439de6ba
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-20 14:04:08 -0300

    Consultando la Rest API para obtener los clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,12 +1,27 @@
-import { Outlet } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const Inicio = () => {
+    const [clientes, setClientes] = useState([])
+
+    useEffect(() => {
+        const obtenerClientesAPI = async () => {
+            try {
+                const url = 'http://localhost:4000/clientes'
+                const respuesta = await fetch(url) /* Puesto que es una petición GET sólo requiere la url */
+                const resultado = await respuesta.json()
+                setClientes(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerClientesAPI()
+    }, [])
+
     return (
         <div>
             <h1>Inicio.jsx</h1>
-            <Outlet />
         </div>
     )
 }
 
-export default Inicio;
+export default Inicio
