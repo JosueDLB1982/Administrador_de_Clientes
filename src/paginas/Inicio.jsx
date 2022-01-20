14b7fdc1c300cb4275eaf14a647722243a836802
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-20 14:32:17 -0300

    Componenete <Cliente/> creado, tabla para mostrar los clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import Cliente from "../components/Cliente"
 
 const Inicio = () => {
     const [clientes, setClientes] = useState([])
@@ -6,7 +7,7 @@ const Inicio = () => {
     useEffect(() => {
         const obtenerClientesAPI = async () => {
             try {
-                const url = 'http://localhost:4000/clientes'
+                const url = 'http://localhost:4000/clientes' /* Con esto vamos a obtener los clientes de la API */
                 const respuesta = await fetch(url) /* Puesto que es una petición GET sólo requiere la url */
                 const resultado = await respuesta.json()
                 setClientes(resultado)
@@ -18,9 +19,30 @@ const Inicio = () => {
     }, [])
 
     return (
-        <div>
-            <h1>Inicio.jsx</h1>
-        </div>
+        <>
+            <h1 className="font-black text-4xl text-blue-900 m">Clientes</h1>
+            <p className="mt-3">Adminístra tus Clientes</p>
+
+            <table className="w-full mt-5 table-auto shadow bg-white">
+                <thead className="bg-blue-800 text-white">
+                    <tr>
+                        <th className="p-2">Nombre</th>
+                        <th className="p-2">Contacto</th>
+                        <th className="p-2">Empresa</th>
+                        <th className="p-2">Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {clientes.map(cliente => (
+                        <Cliente
+                            key={cliente.id}
+                            cliente={cliente}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
 
