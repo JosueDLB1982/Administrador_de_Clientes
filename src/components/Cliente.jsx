4f55f541f8a82af7a8fb3bba6e3441bf0fd03840
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-20 18:37:55 -0300

    Mostrando el listado de clientes

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,10 +1,38 @@
 
 
 const Cliente = ({cliente}) => {
+    const {nombre, telefono, email, empresa, notas, id} = cliente
     return (
-        <div>
-            <h1>{cliente.nombre}</h1>
-        </div>
+        <tr className="border-3 hover:bg-green-100">
+            <td className="p-3 text-center font-bold">{nombre}</td>
+            <td className="p-3">
+                <p><span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
+                <p><span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
+            </td>
+            <td className="p-3">{empresa}</td>
+            <td className="p-3">
+                <button
+                    type="button"
+                    className="bg-purple-600 hover:bg-purple-900 block w-full text-white p-2 rounded-md uppercase font-bold text-xs m-2"
+                >
+                    Ver
+                </button>
+
+                <button
+                    type="button"
+                    className="bg-blue-600 hover:bg-blue-900 block w-full text-white p-2 rounded-md uppercase font-bold text-xs m-2"
+                >
+                    Editar
+                </button>
+
+                <button
+                    type="button"
+                    className="bg-red-600 hover:bg-red-700 block w-full text-white p-2 rounded-md uppercase font-bold text-xs m-2"
+                >
+                    Eliminar
+                </button>
+            </td>
+        </tr>
     )
 }
 
